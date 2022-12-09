5021d6270f0795f3092787468030d156b92a72df
Houston <Peethouston@gmail.com>
2022-12-09 15:42:21 -0600

    fixed one test

--- scriptHelper.js
+++ scriptHelper.js
@@ -19,7 +19,7 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-    if (testInput === '') {
+    if (testInput === "") {
         return "Empty";
     }
     if (isNaN(Number(testInput))) {
